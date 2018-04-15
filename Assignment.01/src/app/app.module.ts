943c08fb365fa9ead070be56084d09c94aca9718
isuru.amarathunga <isuru.amarathunga@cloudsolution-sa.com>
2018-04-15 14:47:37 +0530

    assignment 01 initial checkin

---
+++ Assignment.01/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './SuccessAlert/SuccessAlert.component';
+import { WarningAlertComponent } from './WarningAlert/warningalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
