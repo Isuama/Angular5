0a3bea4cc67c0f22f4e1caa10bfd9444a57e99b1
isuru.amarathunga <isuru.amarathunga@cloudsolution-sa.com>
2018-04-16 18:43:14 +0530

    changes for assignment 2

--- Assignment.02/src/app/login/login.component.ts
+++ Assignment.02/src/app/login/login.component.ts
@@ -9,23 +9,11 @@ export class LoginComponent {
     userName = '';
     printName = '';
     showName = false;
-    hasWord = false;
-
-    onNameChange(event: Event) {
-       this.printName = '';
-       if (!(<HTMLInputElement>event.target).value) {
-        this.hasWord = false;
-       } else {
-           this.hasWord = true;
-       }
-       console.log((<HTMLInputElement>event.target).value);
-    }
 
     onLogin() {
     console.log(event);
      this.printName = 'Hello ' + this.userName + '!!!';
      this.showName = true;
-     this.hasWord = false;
      this.userName = '';
  }
 }
